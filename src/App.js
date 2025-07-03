e9d5ab30e5b232a08b3fed8349ec5c1b7e143ad3
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-07-03 19:26:39 +0300

    Created App.js
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Stocks';
+import Stocks from './pages/Stocks';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-        Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stocks/:symbol" element={<Stocks />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
