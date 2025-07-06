991b6a0447228969c0a8ba46567c2af12c20c21b
mitchellkoech <mitchikoechi3.7@gmail.com>
2025-07-06 17:14:49 +0300

    Created Card component
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,1 +1,17 @@
 // Write your Single Card component here
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Card = ({ symbol }) => {
+    const navigate = useNavigate();
+
+    return(
+        <div className='card'>
+            <div className='card_body'>
+                <h2 className='card_title'>{symbol}</h2>
+            </div>
+            <button className='card_btn' onClick={() => navigate(`/stocks'/:${symbol}` {state: symbol})}>View Details</button>
+            </div>
+    );
+}
+export default Card;
